163d893ed7404b6ae08eb788c24519909692f83f
egorikas <egorgrishechko@gmail.com>
2019-09-02 16:43:00 +0300

    add starting file

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo "Убиваем предыдущий запуск"
+lsof -ti tcp:5000 | xargs kill
+lsof -ti tcp:6000 | xargs kill
+lsof -ti tcp:7000 | xargs kill
+
+echo "Запускаем сервисы"
+
+dotnet run -p First 2>/dev/null &
+dotnet run -p Second 2>/dev/null &
+dotnet run -p Third 2>/dev/null &
+
+echo "Ожидаем старта"
+
+wait
