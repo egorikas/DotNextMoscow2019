aa6f2f9b4bd44c39e7b773af93eba3b176959e89
egorikas <egorgrishechko@gmail.com>
2019-09-10 08:52:16 +0300

    Add writing logs to span

--- Third/Controllers/NumberController.cs
+++ Third/Controllers/NumberController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using OpenTracing;
 
 namespace Third.Controllers
 {
@@ -10,11 +11,23 @@ namespace Third.Controllers
     [ApiController]
     public class NumberController : ControllerBase
     {
+        private readonly ITracer _tracer;
+
+        public NumberController(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+
         // GET api/values
         [HttpGet]
         public async Task<string> Get()
         {
-            return $"третий";
+            var builder = _tracer.BuildSpan("Получить 3 значение");
+            using (var started = builder.StartActive())
+            {
+                started.Span.Log("мы здесь");
+                return $"третий";
+            }
         }
     }
 }
